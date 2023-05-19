a39f6a11707356ff7205c02b4566ca2022f33b96
David Kurilla <kurilla.david@student.greenriver.edu>
2023-05-19 16:47:48 -0400

    Configured index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+
+//Enable error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload.php
+require_once('vendor/autoload.php');
+
+//Create instance of Base
+$f3 = Base::instance();
+
+//Define default route
+$f3->route('GET /', function() {
+
+    //Create instance of Template
+    $view = new Template();
+
+    //Render $view
+    echo $view->render('views/home.html');
+});
