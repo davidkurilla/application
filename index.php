fb0d2bb1206870006f6a88e1bab7b47a8a1f87ea
David Kurilla <kurilla.david@student.greenriver.edu>
2023-05-20 20:28:31 -0400

    Configured routing for all views

--- index.php
+++ index.php
@@ -21,52 +21,98 @@ $f3->route('GET /', function() {
 });
 
 //Define info form route
-$f3->route('GET /info', function() {
+$f3->route('GET|POST /info', function($f3) {
 
     //Create instance of Template
     $view = new Template();
 
     //Render $view
     echo $view->render('views/info.html');
+
+    //Variables
+    $fname = "";
+    $lname = "";
+    $email = "";
+    $state = "";
+    $phone = "";
+
+    //Reroute to experience
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        if(isset($_POST['fname'])) {$fname = $_POST['fname'];}
+        if(isset($_POST['lname'])) {$lname = $_POST['lname'];}
+        if(isset($_POST['email'])) {$email = $_POST['email'];}
+        if(isset($_POST['state'])) {$state = $_POST['state'];}
+        if(isset($_POST['phone'])) {$phone = $_POST['phone'];}
+
+        $f3->set('SESSION.fname', $fname);
+        $f3->set('SESSION.lname', $lname);
+        $f3->set('SESSION.email', $email);
+        $f3->set('SESSION.state', $state);
+        $f3->set('SESSION.phone', $phone);
+
+        $f3->reroute('/experience');
+    }
+
 });
 
-//Reroute to experience
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $f3->reroute('/experience');
-}
-
 //Define experience form route
-$f3->route('GET /experience', function() {
+$f3->route('GET|POST /experience', function($f3) {
 
     //Create instance of Template
     $view = new Template();
 
     //Render $view
     echo $view->render('views/experience.html');
+
+    //Variables
+    $bio = "";
+    $github = "";
+    $years = "";
+    $relocate = "";
+
+    //Reroute to mailing list
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        if(isset($_POST['bio'])) {$bio = $_POST['bio'];}
+        if(isset($_POST['github'])) {$github = $_POST['github'];}
+        if(isset($_POST['years'])) {$years = $_POST['years'];}
+        if(isset($_POST['relocate'])) {$relocate = $_POST['relocate'];}
+
+        $f3->set('SESSION.bio', $bio);
+        $f3->set('SESSION.github', $github);
+        $f3->set('SESSION.years', $years);
+        $f3->set('SESSION.relocate', $relocate);
+
+        $f3->reroute('/mailing_lists');
+    }
 });
 
-//Reroute to mailing list
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $f3->reroute('/mailing_lists');
-}
-
 //Define mailing list form route
-$f3->route('GET /mailing_lists', function() {
+$f3->route('GET|POST /mailing_lists', function($f3) {
 
     //Create instance of Template
     $view = new Template();
 
     //Render $view
     echo $view->render('views/mailing_lists.html');
+
+    //Variables
+    $mailing = array();
+
+    //Reroute to summary
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        if(isset($_POST['mailing'])) {$mailing = $_POST['mailing'];}
+
+        $f3->set('SESSION.mailing', implode(", ", $mailing));
+
+        $f3->reroute('/summary');
+    }
 });
 
-//Reroute to summary
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $f3->reroute('/summary');
-}
-
 //Define summary form route
-$f3->route('GET /summary', function() {
+$f3->route('GET|POST /summary', function($f3) {
 
     //Create instance of Template
     $view = new Template();
